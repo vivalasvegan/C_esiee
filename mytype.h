aab8b1c28ec065372f59c559506856e0780f0cea
Christiane RAKOTOARISOA <Christiane@MacBook-Air-de-Christiane.local>
2018-05-03 23:35:45 +0200

    ajout d'un premier fichier pour tester git

---
+++ mytype.h
@@ -0,0 +1,14 @@
+//
+//  mytype.h
+//  C_projet
+//
+//  Created by Christiane RAKOTOARISOA on 03/05/2018.
+//  Copyright © 2018 Christiane RAKOTOARISOA. All rights reserved.
+//
+
+#ifndef mytype_h
+#define mytype_h
+
+#include <stdio.h>
+
+#endif /* mytype_h */
