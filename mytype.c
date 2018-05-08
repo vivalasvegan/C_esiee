b4ba85578921ae6167fa67d685cb2b1f25d2022e
Christiane RAKOTOARISOA <Christiane@MacBook-Air-de-Christiane.local>
2018-05-08 21:33:05 +0200

    ajout nouveau fichier au projet 8 mai 2018 à 21h30

--- mytype.c
+++ mytype.c
@@ -7,3 +7,60 @@
 //
 
 #include "mytype.h"
+
+Tabdyn.h
+#include "mytype.h"
+typedef Unsigned int Unit;
+
+typedef struct{
+    Uint Size, top;
+    mytype **T;
+}tabDyn;
+
+Struct X{a};
+X x;
+x.a = alpha (mais je ne sais pas comment le noter avec l'ordi)
+             X.y
+             y ->a = alpha (mais je ne sais pas comment le noter avec l'ordi)
+                            
+tabDyn * CONSTRUCTtabDyn () {
+tabdyn of X = malloc (sizeof (Tabdyn));
+X -> T = malloc(sizeof (mytype)*
+    X ->Sie = Size;
+    top = (zéro barré) ;
+    return X;
+}
+                                                
+tabDyn * constructTabDyn();
+void
+destruct TabDyn (tabDyn *);
+                                                
+                
+____________________________________________________________
+mytype.h
+typedef Struct {
+    char * nom;
+    }mytype
+                                                
+                                                
+ mytype * constructMytype (char*);
+ void     destructMytype(mytype*);
+ void     affiche mytype(mytype*);
+ 
+ mytype* constructMytype(char * S)S
+ mytype * K = malloc(Sizeof (mytype));
+ K->nom=malloc(Struct(S)+1)*Sizeof(char));
+ Strcpy(K->nom, S);
+ return K;
+ }
+----------------------------------------------------------------------------
+                            
+char * TROOTS[]= {"coucou", "salut", "hello"};
+int main () {
+tabDyn * TD =constructTabDyn ();
+for ( i = 0; i<Sizeof (char *); i++)
+        ajout (TD, constructMytype(TMots[i]);
+               affich TabDyn(TD);
+               free(TD);
+               return 0;
+               }
